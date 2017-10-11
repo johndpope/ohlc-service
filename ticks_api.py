1e600d45138c0d2030a46f398de25add4a89cd32
Nikita Pavlov <nik.pavlov@outlook.com>
2017-10-11 15:53:32 +0200

    Export and import to csv Print of OHCL graph Project structure changes

--- ticks_api.py
+++ ticks_api.py
@@ -5,13 +5,12 @@ import time
 import urllib.request
 
 from pandas import read_csv
+from pandas import DataFrame
 
 import plotly.offline as py
 import plotly.graph_objs as go
-import plotly.figure_factory as ff
 
 py.init_notebook_mode(connected=True)
-
 
 def contains_error(json):
     if not json["error"]:
@@ -77,9 +76,10 @@ def save_all_trades(pair_name):
     print("pair_name={}".format(pair_name))
     directory = "./result/" + pair_name + "/"
     mkdir(directory)
-    file = open(directory + pair_name + datetime.datetime.now().strftime('_%Y_%m_%d_%H_%M_%S') + ".txt", 'w')
+    file__path = directory + pair_name + datetime.datetime.now().strftime('_%Y_%m_%d_%H_%M_%S') + ".txt"
+    file = open(file__path, 'w')
 
-    while (True):
+    while True:
 
         result, last_id, is_last_request = get_ohlc_data(file, pair_name, since)
 
@@ -95,22 +95,26 @@ def save_all_trades(pair_name):
             print("Unable to get answer, sleep_counter={}".format(sleep_counter))
             time.sleep(sleep_counter)
 
-        if (is_last_request) or error_counter > 8:
+        if is_last_request:
             file.close()
-            return
+            return file__path
 
+        if error_counter > 8:
+            file.close()
+            raise BaseException('Too many errors')
 
-# save_all_trades('LTCUSD')
 
 class Candle:
-    __slots__ = ['open', 'close', 'high', 'low', 'volume',]
 
-    def __init__(self, open=0.0, close= 0.0, high=0.0, low=0.0, volume=0.0):
+    __slots__ = ['open', 'close', 'high', 'low', 'volume', ]
+
+    def __init__(self, open=0.0, close=0.0, high=0.0, low=0.0, volume=0.0):
         self.open = open
         self.close = close
         self.high = high
         self.low = low
         self.volume = volume
+
 
 def build_ohlc(filename):
     file = open(filename, 'r')
@@ -122,25 +126,26 @@ def build_ohlc(filename):
     trade_direction = fisrt_line_words[3]
     # fisrt_trade_market_limit = fisrt_line_words[4]
 
-    date = []
-    open = []
-    close = []
-    high = []
-    low = []
-    volume = []
+    date = {'s': [], 'b': []}
+    open_price = {'s': [], 'b': []}
+    close_price = {'s': [], 'b': []}
+    high_price = {'s': [], 'b': []}
+    low_price = {'s': [], 'b': []}
+    volume = {'s': [], 'b': []}
 
     current_minute = fisrt_trade_datetime.replace(second=0, microsecond=0)
     next_minute = current_minute + datetime.timedelta(minutes=1);
-    ohlc = {'s': [[],[]], 'b': [[],[]]}
+
+    ohlc = {'s': {}, 'b': {}}
 
     ohlc[trade_direction][current_minute] = Candle(trade_price, trade_price, trade_price,
                                                    trade_price, trade_volume)
-    date.append(current_minute)
-    open.append(trade_price)
-    close.append(trade_price)
-    high.append(trade_price)
-    low.append(trade_price)
-    volume.append(trade_volume)
+    date[trade_direction].append(current_minute)
+    open_price[trade_direction].append(trade_price)
+    close_price[trade_direction].append(trade_price)
+    high_price[trade_direction].append(trade_price)
+    low_price[trade_direction].append(trade_price)
+    volume[trade_direction].append(trade_volume)
 
     count = 0
 
@@ -162,18 +167,18 @@ def build_ohlc(filename):
 
         if current_minute not in ohlc[trade_direction]:
             ohlc[trade_direction][current_minute] = Candle(trade_price, trade_price,
-                                                                 trade_price, trade_price,
-                                                                 trade_volume)
+                                                           trade_price, trade_price,
+                                                           trade_volume)
 
-            date.append(current_minute)
-            open.append(trade_price)
-            close.append(trade_price)
-            high.append(trade_price)
-            low.append(trade_price)
-            volume.append(trade_volume)
+            date[trade_direction].append(current_minute)
+            open_price[trade_direction].append(trade_price)
+            close_price[trade_direction].append(trade_price)
+            high_price[trade_direction].append(trade_price)
+            low_price[trade_direction].append(trade_price)
+            volume[trade_direction].append(trade_volume)
 
         else:
-            ohlc[trade_direction][current_minute].volume += trade_volume;
+            ohlc[trade_direction][current_minute].volume += trade_volume
             ohlc[trade_direction][current_minute].close = trade_price
 
             if ohlc[trade_direction][current_minute].low > trade_price:
@@ -182,12 +187,147 @@ def build_ohlc(filename):
             if ohlc[trade_direction][current_minute].high < trade_price:
                 ohlc[trade_direction][current_minute].high = trade_price
 
-        print("Counter={}".format(count))
+            volume[trade_direction][-1] += trade_volume
+            close_price[trade_direction][-1] = trade_price
+
+            if low_price[trade_direction][-1] > trade_price:
+                low_price[trade_direction][-1] = trade_price
+
+            if high_price[trade_direction][-1] < trade_price:
+                high_price[trade_direction][-1] = trade_price
+
+        if count % 1000 == 0:
+            print("Counter={}".format(count))
         count += 1
 
-    return ohlc;
+    ohlc_df = DataFrame({
+        'open': open_price['s'],
+        'close': close_price['s'],
+        'high': high_price['s'],
+        'low': low_price['s'],
+        'volume': volume['s']
+    }, index=date['s'])
+    return ohlc_df;
 
-#def print_ohlc(ohlc):
+def build_ohlc_3(filename):
+    file = open(filename, 'r')
+
+    # <price>, <volume>, <time>, <buy/sell>, <market/limit>, <miscellaneous>
+    first_line_words = file.readline().split('\t')
+
+    if len(first_line_words) < 5:
+        raise Exception('Invalid input file: expected 5 columns row')
+
+    first_trade_datetime = datetime.datetime.strptime(first_line_words[0], '%Y-%m-%d %H:%M:%S.%f')
+    trade_price = float(first_line_words[1])
+    trade_volume = float(first_line_words[2])
+    trade_dir = first_line_words[3]
+    # fisrt_trade_market_limit = fisrt_line_words[4]
+
+    date = {'s': [], 'b': []}
+    open_price = {'s': [], 'b': []}
+    close_price = {'s': [], 'b': []}
+    high_price = {'s': [], 'b': []}
+    low_price = {'s': [], 'b': []}
+    volume = {'s': [], 'b': []}
+    weighted_average = {'s': [], 'b': []}
+
+    current_minute = first_trade_datetime.replace(second=0, microsecond=0)
+    next_minute = current_minute + datetime.timedelta(minutes=1);
+
+    date[trade_dir].append(current_minute)
+    open_price[trade_dir].append(trade_price)
+    close_price[trade_dir].append(trade_price)
+    high_price[trade_dir].append(trade_price)
+    low_price[trade_dir].append(trade_price)
+    volume[trade_dir].append(trade_volume)
+    weighted_average[trade_dir].append(trade_volume * trade_price)
+
+
+    count = 0
+
+    for line in file:
+
+        words = line.split('\t')
+        if len(words) < 5:
+            continue
+
+        try:
+            trade_datetime = datetime.datetime.strptime(words[0], '%Y-%m-%d %H:%M:%S.%f')
+            trade_price = float(words[1])
+            trade_volume = float(words[2])
+            trade_dir = words[3]
+            # trade_market_limit = words[4]
+        except Exception as e:
+            print(e)
+            continue
+
+        if trade_volume == 0:
+            continue
+
+        while trade_datetime >= next_minute:
+            current_minute += datetime.timedelta(minutes=1);
+            next_minute += datetime.timedelta(minutes=1);
+
+        if not date[trade_dir] or date[trade_dir][-1] != current_minute:
+
+            if date[trade_dir]:
+                weighted_average[trade_dir][-1] = weighted_average[trade_dir][-1] / volume[trade_dir][-1]
+
+            date[trade_dir].append(current_minute)
+            open_price[trade_dir].append(trade_price)
+            close_price[trade_dir].append(trade_price)
+            high_price[trade_dir].append(trade_price)
+            low_price[trade_dir].append(trade_price)
+            volume[trade_dir].append(trade_volume)
+            weighted_average[trade_dir].append(trade_volume * trade_price)
+
+        else:
+
+            volume[trade_dir][-1] += trade_volume
+            close_price[trade_dir][-1] = trade_price
+
+            if low_price[trade_dir][-1] > trade_price:
+                low_price[trade_dir][-1] = trade_price
+
+            if high_price[trade_dir][-1] < trade_price:
+                high_price[trade_dir][-1] = trade_price
+
+            weighted_average[trade_dir][-1] += trade_volume * trade_price
+
+        if count % 1000 == 0:
+            print("Counter={}".format(count))
+        count += 1
+
+    if weighted_average['s']:
+        weighted_average['s'][-1] = weighted_average['s'][-1] / volume['s'][-1]
+
+    if weighted_average['b']:
+        weighted_average['b'][-1] = weighted_average['b'][-1] / volume['b'][-1]
+
+    ohlc_bid = DataFrame({
+        'open': open_price['s'],
+        'close': close_price['s'],
+        'high': high_price['s'],
+        'low': low_price['s'],
+        'volume': volume['s'],
+        'weighted_average': weighted_average['s']
+    }, index=date['s'])
+
+    ohlc_ask = DataFrame({
+        'open': open_price['b'],
+        'close': close_price['b'],
+        'high': high_price['b'],
+        'low': low_price['b'],
+        'volume': volume['b'],
+        'weighted_average': weighted_average['b']
+    }, index=date['b'])
+
+    return ohlc_bid, ohlc_ask
+
+
+
+# def print_ohlc(ohlc):
 #    btc_trace = go.Scatter(x=ohlc['s'].keys(), y=ohlc['s'].keys())
 #    py.iplot([btc_trace])
 #    py.offline.plot([btc_trace], filename='file.html')
@@ -196,10 +336,48 @@ def build_ohlc_2(filename, pairname):
     ohlc = read_csv(filename, "\t")
     btc_trace = go.Scatter(x=ohlc['Date'], y=ohlc['price'])
     py.iplot([btc_trace])
-    py.offline.plot([btc_trace], filename=pairname+'.html')
-    
+    py.offline.plot([btc_trace], filename=pairname + '.html')
+
     return ohlc
 
-ohlc_ltc = build_ohlc_2('result/LTCUSD/LTCUSD.txt', 'LTCUSD')
 
-print("OHLC size={}".format(ohlc_ltc['s']))
+def print_ohlc(dir, filename, pairname, mode='lines'):
+
+    ohlc_bid, ohlc_ask = build_ohlc_3(dir+filename)
+
+    ohlc_bid.to_csv(dir + os.path.splitext(filename)[0]+"_bid.csv", sep=';', encoding='utf-8')
+    ohlc_ask.to_csv(dir + os.path.splitext(filename)[0]+"_ask.csv", sep=';', encoding='utf-8')
+
+    bid_graph = go.Scatter(x=ohlc_bid.index, y=ohlc_bid['open'], name='Bid ' + pairname, mode=mode)
+    ask_graph = go.Scatter(x=ohlc_ask.index, y=ohlc_ask['open'], name='Ask ' + pairname, mode=mode)
+    py.iplot([bid_graph, ask_graph])
+    py.offline.plot([bid_graph, ask_graph], filename=dir + pairname + '.html')
+
+    open_bid_graph = go.Scatter(x=ohlc_bid.index, y=ohlc_bid['open'], name='Bid ' + pairname, mode=mode)
+    weighted_average_bid_graph = go.Scatter(x=ohlc_bid.index, y=ohlc_bid['weighted_average'], name='Weight Bid ' + pairname, mode=mode)
+    py.iplot([open_bid_graph, weighted_average_bid_graph])
+    py.offline.plot([open_bid_graph, weighted_average_bid_graph], filename=dir + pairname + '_' + mode + '_weighted.html')
+
+def print_ohlc_from_csv(dir, filename, pairname, mode='lines'):
+
+    ohlc_bid = read_csv(dir + os.path.splitext(filename)[0]+"_bid.csv", sep=';', encoding='utf-8', index_col=0, nrows=10000)
+    #ohlc_ask = read_csv(dir + os.path.splitext(filename)[0]+"_ask.csv", sep=';', encoding='utf-8', index_col=0)
+
+    #bid_graph = go.Scatter(x=ohlc_bid.index, y=ohlc_bid['open'], name='Bid ' + pairname, mode=mode)
+    #ask_graph = go.Scatter(x=ohlc_ask.index, y=ohlc_ask['open'], name='Ask ' + pairname, mode=mode)
+    #py.iplot([bid_graph, ask_graph])
+    #py.offline.plot([bid_graph, ask_graph], filename=dir + pairname + '_' + mode +'.html')
+
+    ohlc_bid['avg'] = ohlc_bid[['high', 'low']].mean(axis=1)
+
+    open_bid_graph = go.Scatter(x=ohlc_bid.index, y=ohlc_bid['open'], name='Open Bid ' + pairname, mode=mode)
+    avg_bid_graph_w = go.Scatter(x=ohlc_bid.index, y=ohlc_bid['avg'], name='Avg Bid ' + pairname, mode=mode)
+    weighted_average_bid_graph = go.Scatter(x=ohlc_bid.index, y=ohlc_bid['weighted_average'], name='Weight Avf Bid ' + pairname, mode=mode)
+    curves = [open_bid_graph, avg_bid_graph_w, weighted_average_bid_graph]
+
+    py.iplot(curves)
+    py.offline.plot(curves, filename=dir + pairname + '_' + mode + '_weighted.html')
+
+#file_path = save_all_trades('LTCUSD')
+print_ohlc_from_csv('result/LTCUSD/', 'LTCUSD_2017_10_11_12_01_03.txt', 'LTCUSD', mode='markers')
+print("Finish")
